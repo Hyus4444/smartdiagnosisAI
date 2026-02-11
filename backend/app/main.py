eb5329193d191363bb00069ec3c4c513fd0d7ea4
Stevan <quirogagonzalezjairostevan@gmail.com>
2026-02-11 11:53:31 -0500

    Minor adjustment to the main.py document
    Minor adjustment to the main.py document by removing the import of the deleted endpoint.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from app.core.database import Base, engine
 from app.models import user, patient
-from app.api import logIn, signIn, patients, clinical_records
+from app.api import logIn, signIn, patients
 
 app = FastAPI(title="SmartDiagnosis AI API", version="1.0.0") 
 
 app.include_router(logIn.router)
 app.include_router(signIn.router)
 app.include_router(patients.router)
-app.include_router(clinical_records.router)
